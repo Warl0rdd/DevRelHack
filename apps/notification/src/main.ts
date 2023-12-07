4685f9a56be31ce13e1be7d1778f906f8f7b4df8
ig <kovriklol@gmail.com>
2023-12-07 22:02:03 +0300

    draft: rabbitmq

--- apps/notification/src/main.ts
+++ apps/notification/src/main.ts
@@ -1,25 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { NotificationModule } from './notification.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe, Logger } from '@nestjs/common';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(NotificationModule);
+  const user = 'user';
+  const password = 'password';
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    NotificationModule,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [`amqp://${user}:${password}@localhost:5672`],
+        queue: 'notification_queue',
+        queueOptions: {
+          durable: false,
+        },
+      },
+    },
+  );
 
-  const swaggerPostfix = 'api/swagger';
-  const config = new DocumentBuilder()
-    .setTitle('Auth Swagger')
-    .setDescription('Notification api')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup(swaggerPostfix, app, document);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  await app.listen(3001);
-
-  const url = await app.getUrl();
-  Logger.verbose(`Application: ${url}`);
-  Logger.verbose(`Swagger: ${url}/${swaggerPostfix}`);
+  await app.listen();
 }
 
 bootstrap();
